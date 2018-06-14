798bdf137630e4bfd82610ed3fb4a51c1b279d54
sue445 <sue445@sue445.net>
2018-06-15 03:43:37 +0900

    WIP

--- spec/rubocop/cop/itamae/node_key_type_spec.rb
+++ spec/rubocop/cop/itamae/node_key_type_spec.rb
@@ -3,21 +3,130 @@
 RSpec.describe RuboCop::Cop::Itamae::NodeKeyType do
   subject(:cop) { described_class.new(config) }
 
-  let(:config) { RuboCop::Config.new }
+  let(:config) { RuboCop::Config.new('Itamae/NodeKeyType' => cop_config) }
+  let(:cop_config) { { 'EnforcedStyle' => enforced_stype } }
 
-  # TODO: Write test code
-  #
-  # For example
-  it 'registers an offense when using `#bad_method`' do
-    expect_offense(<<-RUBY.strip_indent)
-      bad_method
-      ^^^^^^^^^^ Use `#good_method` instead of `#bad_method`.
-    RUBY
+  context 'EnforcedStyle: symbol' do
+    let(:enforced_stype) { 'symbol' }
+
+    context 'with symbol key' do
+      it 'registers no offense' do
+        expect_no_offenses(<<-RUBY.strip_indent)
+          node[:ruby][:version]
+        RUBY
+      end
+    end
+
+    context 'with symbol key (including hyphen)' do
+      it 'registers no offense' do
+        expect_no_offenses(<<-RUBY.strip_indent)
+          node['td-agent'][:version]
+        RUBY
+      end
+    end
+
+    context 'with string key (1st element)' do
+      it 'registers an offense' do
+        expect_offense(<<-RUBY.strip_indent)
+          node['ruby'][:version]
+               ^^^^^^ Use symbol key
+        RUBY
+      end
+
+      include_examples 'autocorrect' do
+        let(:original) do
+          <<-RUBY.strip_indent
+            node['ruby'][:version]
+          RUBY
+        end
+
+        let(:corrected) do
+          <<-RUBY.strip_indent
+            node[:ruby][:version]
+          RUBY
+        end
+      end
+    end
+
+    context 'with string key (2nd element)' do
+      it 'registers an offense' do
+        expect_offense(<<-RUBY.strip_indent)
+          node[:ruby]['version']
+                      ^^^^^^^^^ Use symbol key
+        RUBY
+      end
+
+      include_examples 'autocorrect' do
+        let(:original) do
+          <<-RUBY.strip_indent
+            node[:ruby]['version']
+          RUBY
+        end
+
+        let(:corrected) do
+          <<-RUBY.strip_indent
+            node[:ruby][:version]
+          RUBY
+        end
+      end
+    end
   end
 
-  it 'does not register an offense when using `#good_method`' do
-    expect_no_offenses(<<-RUBY.strip_indent)
-      good_method
-    RUBY
+  context 'EnforcedStyle: string' do
+    let(:enforced_stype) { 'string' }
+
+    context 'with string key' do
+      it 'registers no offense' do
+        expect_no_offenses(<<-RUBY.strip_indent)
+          node['ruby']['version']
+        RUBY
+      end
+    end
+
+    context 'with symbol key (1st element)' do
+      it 'registers an offense' do
+        expect_offense(<<-RUBY.strip_indent)
+          node[:ruby]['version']
+               ^^^^^ Use string key
+        RUBY
+      end
+
+      include_examples 'autocorrect' do
+        let(:original) do
+          <<-RUBY.strip_indent
+            node[:ruby]['version']
+          RUBY
+        end
+
+        let(:corrected) do
+          <<-RUBY.strip_indent
+            node['ruby']['version']
+          RUBY
+        end
+      end
+    end
+
+    context 'with symbol key (2nd element)' do
+      it 'registers an offense' do
+        expect_offense(<<-RUBY.strip_indent)
+          node['ruby'][:version]
+                       ^^^^^^^^ Use string key
+        RUBY
+      end
+
+      include_examples 'autocorrect' do
+        let(:original) do
+          <<-RUBY.strip_indent
+            node['ruby'][:version]
+          RUBY
+        end
+
+        let(:corrected) do
+          <<-RUBY.strip_indent
+            node['ruby']['version']
+          RUBY
+        end
+      end
+    end
   end
 end
